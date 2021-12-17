8dafaa85c52dbad58ea7beeb14a9e7ff5856917d
ijurao <ijurao@gmail.com>
2021-12-18 04:31:11 +1100

    refactor one method

--- Stars/Program.cs
+++ Stars/Program.cs
@@ -7,30 +7,21 @@ namespace Stars
     {
         static void Main(string[] args)
         {
-            DrawStars(33);
+            DrawStars(64);
             Console.WriteLine("Hello World!");
         }
 
         static void DrawStars(int n)
         {
-            for (int i = n; i >= 0; i-=2)
-            {
-                var times = i == 0 ? 1 : i;
-                int amoutOfBlankSpaces =  ( (n - i) / 2);
-                var s = string.Concat(Enumerable.Repeat("*", times));
-               
-                if(amoutOfBlankSpaces > 0)
-                {
-                    if (amoutOfBlankSpaces == n / 2 && n % 2 == 0) amoutOfBlankSpaces--;
-                    string blanks = string.Concat(Enumerable.Repeat(" ", amoutOfBlankSpaces));
-                    s = s.Insert(0, blanks);
-                    s = s.Insert(s.Length, blanks);
-                }
-                
-                Console.WriteLine(s);
-            }
-            
-            for (int i = 1; i <= n; i += 2)
+            DrawFormStars(n,n,false);
+            DrawFormStars(n, 1, true);
+        }
+
+
+        private static void DrawFormStars(int n, int index, bool reverse)
+        {
+
+            for (int i = index; (reverse ?  i<=n : i>=0); i = (reverse ? i += 2 : i -= 2))
             {
                 var times = i == 0 ? 1 : i;
                 int amoutOfBlankSpaces = ((n - i) / 2);
@@ -38,6 +29,7 @@ namespace Stars
 
                 if (amoutOfBlankSpaces > 0)
                 {
+                    if (amoutOfBlankSpaces == n / 2 && n % 2 == 0 && !reverse) amoutOfBlankSpaces--;
                     string blanks = string.Concat(Enumerable.Repeat(" ", amoutOfBlankSpaces));
                     s = s.Insert(0, blanks);
                     s = s.Insert(s.Length, blanks);
